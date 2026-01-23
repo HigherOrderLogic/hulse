4baa6e942edb9c127256c9b4291ac644b4582208
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-01-23 11:55:33 +0700

    refactor(rust): remove `pkgs`

--- rust.nix
+++ rust.nix
@@ -1,6 +1,7 @@
 {
   lib,
-  pkgs,
+  mkShell,
+  stdenv,
   rust-overlay,
   rustc,
   cargo,
@@ -8,13 +9,13 @@
   cargo-insta,
   withInsta ? false,
 }:
-pkgs.mkShell {
+mkShell {
   packages =
     [
       rustc
       cargo
       clippy
-      rust-overlay.packages.${pkgs.stdenv.hostPlatform.system}.rust-nightly.availableComponents.rustfmt
+      rust-overlay.packages.${stdenv.hostPlatform.system}.rust-nightly.availableComponents.rustfmt
     ]
     ++ (lib.optional withInsta cargo-insta);
 }
